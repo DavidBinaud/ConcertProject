fca9b34f4cf6e9ccb1ccfc9424debb7d6c29230c
David Binaud <david.binaud@etu.umontpellier.fr>
2022-02-07 08:46:54 +0100

    Concert fixtures, add more concert to display pagination for next concerts

--- src/DataFixtures/ConcertFixtures.php
+++ src/DataFixtures/ConcertFixtures.php
@@ -11,40 +11,41 @@ class ConcertFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $concert = new Concert();
-        $concert->setCapacity(50)
-            ->setName("Loolapalooza")
-            ->setDate(\DateTime::createFromFormat("d/m/Y", "25/12/2021"))
-            ->setVenue($this->getReference(VenueFixtures::VENUE_REF . 0))
-            ->addBand($this->getReference(BandFixtures::BAND_REF . 0))
-            ->addBand($this->getReference(BandFixtures::BAND_REF . 1));
-        $manager->persist($concert);
+        foreach (["Loolapalooza", "Electrobeach", "Rock en scène", "Les Vieilles Charrues"] as $a){
+            for ($i = 20; $i < 30; $i++) {
+                $concert = new Concert();
+                $concert->setCapacity(50)
+                    ->setName("$a 20$i")
+                    ->setDate(\DateTime::createFromFormat("d/m/Y", rand(1,28) . '/' . rand(1,12) . "/20" . $i))
+                    ->setVenue($this->getReference(VenueFixtures::VENUE_REF . rand(0,4)));
+                for ($j = 0; $j < rand(0,3); $j++) {
+                    $concert->addBand($this->getReference(BandFixtures::BAND_REF . rand(0,8)));
+                }
+                $manager->persist($concert);
+            }
+        }
+
+
 
         $concert = new Concert();
         $concert->setCapacity(500)
-            ->setName("Les Déferlantes")
-            ->setDate(\DateTime::createFromFormat("d/m/Y", "06/01/2022"))
+            ->setName("Woodstock")
+            ->setDate(\DateTime::createFromFormat("d/m/Y", "06/01/1969"))
             ->setVenue($this->getReference(VenueFixtures::VENUE_REF . 0))
             ->addBand($this->getReference(BandFixtures::BAND_REF . 2));
         $manager->persist($concert);
 
-        $concert = new Concert();
-        $concert->setCapacity(500)
-            ->setName("Les Estivales")
-            ->setDate(\DateTime::createFromFormat("d/m/Y", "07/07/2022"))
-            ->setVenue($this->getReference(VenueFixtures::VENUE_REF . 1))
-            ->addBand($this->getReference(BandFixtures::BAND_REF . 2));
-        $manager->persist($concert);
-
-        $concert = new Concert();
-        $concert->setCapacity(500)
-            ->setName("Rock en Scène")
-            ->setDate(\DateTime::createFromFormat("d/m/Y", "17/07/2022"))
-            ->setVenue($this->getReference(VenueFixtures::VENUE_REF . 4))
-            ->addBand($this->getReference(BandFixtures::BAND_REF . 7))
-            ->addBand($this->getReference(BandFixtures::BAND_REF . 5))
-            ->addBand($this->getReference(BandFixtures::BAND_REF . 8));
-        $manager->persist($concert);
+        for ($i = 10; $i < 15; $i++) {
+            $concert = new Concert();
+            $concert->setCapacity(500)
+                ->setName("Les Estivales 20$i")
+                ->setDate(\DateTime::createFromFormat("d/m/Y", rand(1,28) . '/' . rand(1,12) . "/20" . $i))
+                ->setVenue($this->getReference(VenueFixtures::VENUE_REF . rand(0,4)));
+            for ($j = 0; $j < rand(0,3); $j++) {
+                $concert->addBand($this->getReference(BandFixtures::BAND_REF . rand(0,8)));
+            }
+            $manager->persist($concert);
+        }
 
         $manager->flush();
     }
